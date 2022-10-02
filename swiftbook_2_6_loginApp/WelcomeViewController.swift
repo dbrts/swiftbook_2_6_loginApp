f1736107c3635ce41bceed207fbf571fe652c1c5
Gennady Dobretsov <dobrets@gmail.com>
2022-10-02 21:46:41 +0600

    bold welcome text

--- swiftbook_2_6_loginApp/WelcomeViewController.swift
+++ swiftbook_2_6_loginApp/WelcomeViewController.swift
@@ -14,11 +14,24 @@ class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setGradientBackground()
         welcomeLabel.text = "Welcome, \(username ?? "user")!"
     }
 
     @IBAction func logoutButtonPressed() {
         dismiss(animated: true)
+    }
+    
+    private func setGradientBackground() {
+        let colorTop = UIColor(red: 236.0/255.0, green: 110.0/255.0, blue: 173.0/255.0, alpha: 1.0).cgColor
+        let colorBottom =  UIColor(red: 52.0/255.0, green: 148.0/255.0, blue: 230.0/255.0, alpha: 1.0).cgColor
+                    
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [colorTop, colorBottom]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.frame = self.view.bounds
+                
+        self.view.layer.insertSublayer(gradientLayer, at:0)
     }
     
 }
