cd618805414d0a6ade5c0a41a888fe04d7b3e471
Gennady Dobretsov <dobrets@gmail.com>
2022-10-05 22:18:07 +0600

    tab bar appearance

---
+++ swiftbook_2_6_loginApp/TabBarViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TabBarViewController.swift
+//  swiftbook_2_6_loginApp
+//
+//  Created by dobrets on 05.10.2022.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let tabBarAppearance = UITabBarAppearance()
+        tabBarAppearance.configureWithOpaqueBackground()
+        tabBar.standardAppearance = tabBarAppearance
+        tabBar.scrollEdgeAppearance = tabBarAppearance
+    }
+
+}
